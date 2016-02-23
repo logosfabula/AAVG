a4001b90dbf441843d53e5dbb2b31da18ab15fe7
logosfabula <logosfabula@gmail.com>
2016-02-23 19:31:19 +0100

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -3,15 +3,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author logosfabula
+ * @author AAVG
  */
 class Hand {
     List<Card> cards = new ArrayList<>();
