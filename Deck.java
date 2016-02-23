f241b40237ba28d53acdbb086c89d9d934d5c3ba
logosfabula <logosfabula@gmail.com>
2016-02-23 19:30:31 +0100

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -3,15 +3,9 @@ import java.util.Collections;
 import java.util.Stack;
 import java.util.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author logosfabula
+ * @author AAVG
  */
 class Deck {
     Stack<Card> cards = new Stack<>();
