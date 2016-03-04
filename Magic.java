b7877c8474e52931897a9a219799d01894bf2d50
logosfabula <logosfabula@gmail.com>
2016-03-05 00:19:04 +0100

    Update Magic.java

--- Magic.java
+++ Magic.java
@@ -10,7 +10,7 @@ public class Magic {
     public static void main(String[] args) {
         // TODO code application logic here
         Game game = Game.game;
+        game.setup();
         game.start();
-    }
-    
+    }  
 }
