d4ec11d4df5a754aa677db8ee3f189b2678d7601
logosfabula <logosfabula@gmail.com>
2016-03-05 00:29:38 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /**
  *
- * @author AAVG
+ * @author logosfabula
  */
 class Board {
+    List<Creature> creatures = new ArrayList<>();
     
+    public List getCreatures(){
+        return creatures;
+    }
 }
