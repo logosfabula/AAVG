fde9d48505b4a9ef91d4bcb3a6d98acba9f77af2
GEMO95 <gemo95@live.it>
2016-03-04 11:32:29 +0100

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,17 @@
+
+import java.util.Stack;
+import java.util.*;
+
+/**
+ *
+ * @author AAVG
+ */
+
+public class Stack {
+    void push (E elem){  
+      push (elem);
+    }
+    E pop (){
+      pop();
+    }
+}
