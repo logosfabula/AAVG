e2def5afa8d984fb69bd7e3f053816e0c2b21752
logosfabula <logosfabula@gmail.com>
2016-03-05 00:22:27 +0100

    Create InstantSpell.java

---
+++ InstantSpell.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author logosfabula
+ */
+public abstract class InstantSpell implements Castable {
+    @Override
+    public void cast() {}
+}
